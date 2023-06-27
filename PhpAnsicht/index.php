6c835ae0e1b5aa080c85f8d12e16709e6cf47487
Sutoroberii <marenschaa@gmail.com>
2023-06-28 01:51:02 +0200

    Neues Design der Seite
    Neues Design
    Anlegen von katzen und uhrzeiten möglich

--- PhpAnsicht/index.php
+++ PhpAnsicht/index.php
@@ -3,72 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Katzenfutterspender Website</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles/startseite.css">
+    <link rel="stylesheet" type="text/css" href="styles/input.css">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+
 </head>
 <body>
+    <div class = "container2">
+        <div class ="centered">
+            <div class="oben">
+                <input type="button" class="btn-hover color-10" value ="Katze anlegen" onClick="document.location.href='katze.php'">
+            </div>
+            <div class="mitte">
+                <input type="submit" class="btn-hover color-10" value ="Uhrzeit anlegen" onClick="document.location.href='uhrzeit.php'">
 
-<?php
-    //Datenbankverbindung
-    $servername = "127.0.0.1"; 
-    $username = "root";
-    $dbname = "Futterspender";
-
-    $conn = new mysqli($servername, $username,"",$dbname);
-
-    if($conn->connect_error) {
-        die("Verbindung fehlgeschlagen: " . $conn -> connect_error);
-    }
-
-    echo "Verbindung zur Datenbank hergestellt";
-
-    if(isset($_POST['catsubmit'])) {
-        $catvalue = $_POST['catname'];
-        $maxAnzahl = $_POST['anzAusgabe'];
-        $sql = "Insert Into katze (name, maxFutterausgabe) Values ('$catvalue', $maxAnzahl)";
-
-        if ($conn->query($sql) == TRUE) {
-            echo "Neuer Eintrag erstellt"; 
-        }else {
-            echo "Fehler beim Erstellen des Eintrags: " . $conn->error; 
-        }
-    }
-
-    if(isset($_POST['uhrzeitsubmit'])) {
-        $uhrzeit =$_POST['uhrzeit'];
-        $sql = "Insert Into festeausgabe (uhrzeit) Values (TIME('$uhrzeit'))";
-
-        if ($conn->query($sql) == TRUE) {
-            echo "Neuer Eintrag erstellt"; 
-        }else {
-            echo "Fehler beim Erstellen des Eintrags: " . $conn->error; 
-        }
-    }
-?>
-
-<form id = "catsubmit" method="post">
-    <div id="container">
-        <div id="left-div">
-        <h2>Katzen Daten</h2>
-        <label>Katzenname</label>
-        <input type="text"  id="catname" name="catname">
-        <br>
-        <label>Anzahl extra Futterausgabe</label>
-        <input type="number" id="anzAusgabe" name="anzAusgabe">
-        <br>
-        <input type="submit" name="catsubmit" id="catsubmit" value="Senden"> 
-
+            </div>
+                    
+        <div class= "unten"> 
+            <input type="submit" class="btn-hover color-10" value ="Ausgabe Daten"> 
+        
         </div>
-        <div id="right-div">
-        <h2>Allgemeine Futterzeiten</h2>
-        <label>Uhrzeit zur Futterausgabe eingeben</label>
-        <input type="time" placeholder="Uhrzeit eingeben" id="uhrzeit" name="uhrzeit">
-        <br>
-        <input type="submit" name="uhrzeitsubmit" id="uhrzeitsubmit" value="Senden"> 
         </div>
     </div>
-</form>
-
-    
 </body>
 </html>
--- PhpAnsicht/uhrzeit.php
+++ PhpAnsicht/uhrzeit.php
@@ -21,30 +21,27 @@
         die("Verbindung fehlgeschlagen: " . $conn -> connect_error);
     }
 
-    if(isset($_POST['catsubmit'])) {
-        $catvalue = $_POST['catname'];
-        $maxAnzahl = $_POST['anzAusgabe'];
-        $sql = "Insert Into katze (name, maxFutterausgabe) Values ('$catvalue', $maxAnzahl)";
+    if(isset($_POST['uhrzeitsubmit'])) {
+        $uhrzeit =$_POST['uhrzeit'];
+        $sql = "Insert Into festeausgabe (uhrzeit) Values (TIME('$uhrzeit'))";
 
-        if ($conn->query($sql) == false) {
+        if ($conn->query($sql) == FALSE) {
             echo "Fehler beim Erstellen des Eintrags: " . $conn->error; 
         }
     }
 
-
     ?>
+
+
     <form id = "catsubmit" method="post">
         <div class = "container2">
             <div class ="centered">
                 <div class ="test">
-                    <h1>Katzen Daten</h1>
-                    <label>Katzenname</label>
-                    <input type="text"  required class="search-box" name="catname">
+                    <h1>Uhrzeit</h1>
+                    <label>Uhrzeit</label>
+                    <input type="time"  required class="search-box" id="uhrzeit" name="uhrzeit">
                     <br>
-                    <label>Anzahl extra Futterausgabe</label>
-                    <input type="number" id="anzAusgabe" required class="search-box" name="anzAusgabe">
-                    <br>
-                    <input type="submit" name="catsubmit" class="btn-hover color-11"  id="catsubmit"  value="Senden"> 
+                    <input type="submit" name="uhrzeitsubmit" id="uhrzeitsubmit" value="Senden" class="btn-hover color-11"> 
                     <input type="button" class="btn-hover color-11" value ="Einträge Anzeigen">
                     <input type="button" class="btn-hover color-11" value ="Zurück" onClick="document.location.href='index.php'">
                 </div>
